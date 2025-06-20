82372d108e4f48c1f882e0bd2fe707a4715defc8
oludayoadeoye35 <oludayoadeoye35@gmail.com>
2025-06-20 07:19:13 +0000

    gdgdgdgd

---
+++ src/src/environments/environment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCpaMDFFE3cOE70Dzr9kuyRNJid8FIjnlE",
+  authDomain: "projectkanban-6e0e0.firebaseapp.com",
+  projectId: "projectkanban-6e0e0",
+  storageBucket: "projectkanban-6e0e0.firebasestorage.app",
+  messagingSenderId: "353669804050",
+  appId: "1:353669804050:web:436e0dafb51ef6278468cb",
+  measurementId: "G-JBV2Q9LD42"
+};
